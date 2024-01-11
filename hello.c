28c62f4b8d80e7d93a32c2db415e13a2923ea88f
Doa Ahmed <145981694+Doaahmed31@users.noreply.github.com>
2024-01-11 19:32:14 +0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -45,3 +45,54 @@ int main() {
 
     fclose(file);
     curl_easy_cleanup(curl);
+FILE *json_file = fopen("response_data.json", "r");
+    if (!json_file) {
+        fprintf(stderr, "Error opening the JSON file.\n");
+        return 1;
+    }
+
+    
+   // Read the JSON file and parse it
+    json_error_t error;
+    json_t *root = json_load_file("response_data.json", 0, &error);
+    if (!root) {
+        fprintf(stderr, "Error parsing JSON data: %s\n", error.text);
+        return 1;
+    }
+
+    // Extracting weather description 
+    json_t *weather = json_object_get(root, "weather");
+    if (json_array_size(weather) > 0) {
+        json_t *weather_data = json_array_get(weather, 0);
+        json_t *description = json_object_get(weather_data, "description");
+        if (description && json_typeof(description) == JSON_STRING) {
+            printf("Weather Description: %s\n", json_string_value(description));
+        }
+    }
+
+    
+    json_t *main_data = json_object_get(root, "main");
+    json_t *temperature = json_object_get(main_data, "temp");
+    json_t *humidity = json_object_get(main_data, "humidity");
+    json_t *pressure = json_object_get(main_data, "pressure");
+    json_t *visibility = json_object_get(root, "visibility");
+    json_t *feels_like = json_object_get(main_data,"feels_like");
+    json_t *mint = json_object_get(main_data,"temp_min");
+    json_t *maxt = json_object_get(main_data,"temp_max");
+    json_t *cloud = json_object_get(root, "clouds");
+    json_t *percent = json_object_get(cloud, "all");
+    json_t *sys = json_object_get(root,"sys");
+    json_t *sunrise = json_object_get(sys, "sunrise");
+    json_t *sunset = json_object_get(sys, "sunset");
+    json_t *country = json_object_get(sys, "country");
+    json_t *city = json_object_get(root, "name");
+    json_t *wind = json_object_get(root, "speed");
+  
+    //printing
+    printf("%s/%s \n",json_string_value(city),json_string_value(country));
+    printf("pressure : %lld  \n",json_integer_value(pressure));
+    printf("humidity : %lld percent \n",json_integer_value(humidity));
+    printf("visibility : %lld km \n",json_integer_value(visibility)/1000);
+    printf("Temperature: %.2f Celsius\n", json_real_value(temperature) - 273.15); 
+    printf("feels like : %.2f Celcius\n",json_real_value(feels_like) - 273.15);
+    printf("%.2f / %.2f \n" ,json_real_value(mint) - 273.15,json_real_value(maxt) - 273.15);
